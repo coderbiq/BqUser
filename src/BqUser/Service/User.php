35b39e70ca2ff162a990814645d8a83eec0efb47
Elvis.bi <elvis@dwenzi.com>
2012-12-04 23:34:10 +0800

    修改用户服务的类型

--- src/BqUser/Service/User.php
+++ src/BqUser/Service/User.php
@@ -1,8 +1,18 @@
 <?php
 namespace BqUser\Service;
 
-use BqCore\Service\AbstractService;
+use BqCore\Service\AbstractTableService;
+use BqUser\Entity\User as UserEntity;
+use BqUser\Db\Table\User as UserTable;
 
-class User extends AbstractService
+class User extends AbstractTableService
 {
+    public function createEntity() {
+        $userEntity = new UserEntity('id', $this->getTable(), 
+            $this->getAdapter());
+        return $userEntity;
+    }
+
+    public static function getTableName() { return 'bquser_user'; }
+    public static function getAdapterServiceName() { return 'User\Db\Adapter'; }
 }
