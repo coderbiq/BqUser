5c5d21451ddc114ac98b779f1397ba5713bee30f
elvis bi <elvis@dwenzi.com>
2012-12-15 19:43:48 +0800

    完善用户的帐户管理模型

--- src/BqUser/Event/Listener/User.php
+++ src/BqUser/Event/Listener/User.php
@@ -4,13 +4,9 @@ namespace BqUser\Event\Listener;
 use Zend\EventManager\EventManagerInterface;
 use BqCore\Event\Listener\AbstractListener;
 use BqCore\Event\EntityEvent;
-use BqUsre\Entity\UserInterface;
-use BqUser\Entity\AccountInterface;
 
 class User extends AbstractListener
 {
-    protected $bindTableName;
-
     public function attach(EventManagerInterface $events) {
         $this->listeners[] = $events->attach(
             EntityEvent::EVENT_ADD_RELYON_ENTITY,
@@ -23,29 +19,16 @@ class User extends AbstractListener
         $relyonEntity = $event->getRelyonEntity();
         if($target instanceof UserInterface 
             && $relyonEntity instanceof AccountInterface) {
-            $this->bindAccount($target, $relyonEntity);
+
+            $config = $this->getServiceLocator()->get('BqUser\Config')
+                ->get('account_manager');
+            $adapterName = $config->get('adapter');
+            $option = $config->get('options');
+
+            $accountManager = new $adapterName($options);
+            $accountManager->setUser($target)
+                ->setServiceLocator($this->getServiceLocator())
+                ->bindAccount($relyonEntity);
         }
-    }
-
-    public function getBindTableName() { return $this->bindTableName; }
-    public function setBindTableName($tableName) {
-        $this->bindTableName = $tableName;
-        return $this;
-    }
-
-    protected function bindAccount(UserInterface $user, 
-        AccountInterface $account) {
-        $row = new RowGateway('id', $this->getBindTable()->getTable(), 
-            $this->getBindTable()->getAdapter());
-        $row->userId = $user->getId();
-        $row->accountId = $account->getId();
-        $row->accountEntityName = $account->getEntityName();
-        $row->created = time();
-        $row->save();
-    }
-
-    protected function getBindTable() {
-        return new TableGateway($this->getBindTableName(), 
-            $this->getServiceLocator()->get('BqUser\Db\Adapter'));
     }
 }
